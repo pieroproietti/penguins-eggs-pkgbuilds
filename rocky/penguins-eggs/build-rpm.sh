96b056dcc7f0440769b38fb0468844d20c7aea16
Piero Proietti <piero.proietti@gmail.com>
2024-12-05 21:18:54 +0100

    rock

--- rocky/penguins-eggs/build-rpm.sh
+++ rocky/penguins-eggs/build-rpm.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# check if we are on rocky
+# check if we are on rocky/almalinux
 if [ ! -f /etc/rocky-release ]; then
-    echo "This script is only for rocky"
-    exit
+    if [ ! -f /etc/almalinux-release ]; then
+        echo "This script is intended for rockylinux or almalinux!"
+        exit 1
+    fi
 fi
 
 # check if we are root
