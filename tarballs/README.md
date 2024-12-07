41a8d0d30f307f69b09ab12192091d41267cde52
Piero Proietti <piero.proietti@gmail.com>
2024-12-07 18:42:06 +0100

    added tarballs setup

---
+++ tarballs/README.md
@@ -0,0 +1,6 @@
+# eggs tarballs
+
+Installa la tarball di penguins-eggs, funziona sulla maggior parte delle distribuzioni.
+
+## install
+`setup ./penguins-eggs-v10.0.54-1-linux-x64.tar.xz`
---
+++ tarballs/requirements/opensuse/install.sh
@@ -0,0 +1,37 @@
+!#/bin/bash
+
+# check if we are on opensuse
+if [ ! -f /etc/os-release ]; then
+    echo "This script is only for OpenSUSE"
+    exit
+fi
+
+
+# check if we are root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+zypper install -y \
+    bash-completion \
+    cryptsetup \
+    curl \
+    dosfstools \
+    dracut \
+    dracut-tools \
+    fuse \
+    git \
+    jq \
+    lsb-release \
+    lvm2 \
+    parted \
+    rsync \
+    squashfs \
+    sshfs \
+    wget \
+    xdg-user-dirs \
+    xorriso
+
+# Enable uinput
+echo "uinput" | tee /etc/modules-load.d/uinput.conf
