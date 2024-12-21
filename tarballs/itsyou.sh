7786fc2bd6b0c43d4545916af1b4d3c3f76c8e2a
Piero Proietti <piero.proietti@gmail.com>
2024-12-21 14:04:03 +0100

    get-version

--- tarballs/itsyou.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -z "$1" ]; then
-    VERSION="10.0.56"
-else
-    VERSION="$1"
-fi
-VERSIONS=$1
-wget https://penguins-eggs.net/basket//packages/tarballs/penguins-eggs-tarball-${VERSIONS}-1-linux-x64.tar.gz
